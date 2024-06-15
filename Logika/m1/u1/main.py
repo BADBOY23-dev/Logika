18b8185b25496402ae11eadf5ddcf3bc57a26fd3
BADBOY23-dev <monolitstalker2825@gmail.com>
2024-06-15 17:26:49 +0300

    Add files via upload

--- Logika/m1/u1/main.py
+++ Logika/m1/u1/main.py
@@ -5,7 +5,7 @@ from random import randint
 app = QApplication([])
 
 main_win = QWidget()
-main_win.setWindowTitle('PYTHON')
+main_win.setWindowTitle('Logika')
 main_win.move(100, 100)
 main_win.resize(400, 200)
 
