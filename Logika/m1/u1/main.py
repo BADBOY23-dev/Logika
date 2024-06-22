e808cf7bb3a1e331789e780bec8bc233156a0f1e
BADBOY23-dev <monolitstalker2825@gmail.com>
2024-06-22 17:27:06 +0300

    Add files via upload

--- Logika/m1/u1/main.py
+++ Logika/m1/u1/main.py
@@ -5,13 +5,26 @@ from random import randint
 app = QApplication([])
 
 main_win = QWidget()
-main_win.setWindowTitle('Logika')
-main_win.move(100, 100)
+main_win.setWindowTitle('Визначник переможця')
+main_win.move(900, 70)
 main_win.resize(400, 200)
 
 button = QPushButton('Згенерувати')
-text = QLabel('Натисніть щоб згенерувати рандомне число')
+text = QLabel('Натисніть, щоб визначити переможця')
+winner = QLabel('?')
 
+line = QVBoxLayout()
+line.addWidget(text, alignment=Qt.AlignCenter)
+line.addWidget(winner, alignment=Qt.AlignCenter)
+line.addWidget(button, alignment=Qt.AlignCenter)
+main_win.setLayout(line)
+
+def show_winner():
+    number = randint(0, 99)
+    winner.setText(str(number))
+    text.setText('Переможець:')
+
+button.clicked.connect(show_winner)
 
 main_win.show()
 app.exec_() 
